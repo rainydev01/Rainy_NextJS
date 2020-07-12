b37eb079d8f04e3efc155579bc5d51c61f28c4eb
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-07-12 20:30:11 +0530

    gallery completed

--- pages/gallery.js
+++ pages/gallery.js
@@ -10,6 +10,14 @@ import Carousel, { Modal, ModalGateway } from 'react-images';
 export default function Home() {
 	const [ currentImage, setCurrentImage ] = useState(0);
 	const [ viewerIsOpen, setViewerIsOpen ] = useState(false);
+	const [ selectedView, setSelectedView ] = useState(1);
+
+	let gallerySelectedComponent = null;
+	let basicCSS = 'btn btn-default filter-button';
+
+	const onChange = (value) => {
+		setSelectedView(parseInt(value));
+	};
 
 	const openLightbox = useCallback((event, { photo, index }) => {
 		setCurrentImage(index);
@@ -29,7 +37,7 @@ export default function Home() {
 		},
 		{
 			src: 'static/images/gallery/Image2.png',
-			width: 4,
+			width: 5,
 			height: 3
 		},
 		{
@@ -53,6 +61,158 @@ export default function Home() {
 			height: 3
 		}
 	];
+
+	let basicCSS1 = 'btn btn-default filter-button ';
+	let basicCSS2 = 'btn btn-default filter-button ';
+	let basicCSS3 = 'btn btn-default filter-button ';
+	let basicCSS4 = 'btn btn-default filter-button ';
+	let basicCSS5 = 'btn btn-default filter-button ';
+	let basicCSS6 = 'btn btn-default filter-button ';
+	let basicCSS7 = 'btn btn-default filter-button ';
+	let basicCSS8 = 'btn btn-default filter-button ';
+	let basicCSS9 = 'btn btn-default filter-button ';
+	let basicCSS10 = 'btn btn-default filter-button ';
+	let basicCSS11 = 'btn btn-default filter-button ';
+	let basicCSS12 = 'btn btn-default filter-button ';
+	let basicCSS13 = 'btn btn-default filter-button ';
+	let basicCSS14 = 'btn btn-default filter-button ';
+	let basicCSS15 = 'btn btn-default filter-button ';
+
+	if (selectedView === 1) {
+		basicCSS1 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<Gallery photos={photos} onClick={openLightbox} />
+				<ModalGateway>
+					{viewerIsOpen ? (
+						<Modal onClose={closeLightbox}>
+							<Carousel
+								currentIndex={currentImage}
+								views={photos.map((x) => ({
+									...x,
+									srcset: x.srcSet,
+									caption: x.title
+								}))}
+							/>
+						</Modal>
+					) : null}
+				</ModalGateway>
+			</Fragment>
+		);
+	}
+
+	if (selectedView === 2) {
+		basicCSS2 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image1.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 3) {
+		basicCSS3 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image2.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 4) {
+		basicCSS4 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image3.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 5) {
+		basicCSS5 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image4.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 6) {
+		basicCSS6 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image5.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 7) {
+		basicCSS7 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image6.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 8) {
+		basicCSS8 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image1.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 9) {
+		basicCSS9 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image2.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 10) {
+		basicCSS10 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image1.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 11) {
+		basicCSS11 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image3.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 12) {
+		basicCSS12 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image4.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 13) {
+		basicCSS13 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image5.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 14) {
+		basicCSS14 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image6.png" />
+			</Fragment>
+		);
+	}
+	if (selectedView === 15) {
+		basicCSS15 = 'btn btn-default filter-button filter-button-active';
+		gallerySelectedComponent = (
+			<Fragment>
+				<img className=" center-block" src="static/images/gallery/Image4.png" />
+			</Fragment>
+		);
+	}
 
 	return (
 		<Fragment>
@@ -90,52 +250,139 @@ export default function Home() {
 							<div className="row">
 								<div class="col-md-12 glrymnu" align="center">
 									<button
-										className="btn btn-default filter-button filter-button-active"
+										onClick={() => {
+											onChange(1);
+										}}
+										className={basicCSS1}
 										data-filter="all"
 										style={{ padding: '5px 45px' }}
 									>
 										All
 									</button>
-									<button className="btn btn-default filter-button" data-filter="hdpe">
+									<button
+										onClick={() => {
+											onChange(2);
+										}}
+										className={basicCSS2}
+										data-filter="hdpe"
+									>
 										Rooftop RWH
 									</button>
-									<button className="btn btn-default filter-button" data-filter="hdpe">
+									<button
+										onClick={() => {
+											onChange(3);
+										}}
+										className={basicCSS3}
+										data-filter="hdpe"
+									>
 										Groundwater Recharging
 									</button>
-									<button className="btn btn-default filter-button" data-filter="sprinkle">
+									<button
+										onClick={() => {
+											onChange(4);
+										}}
+										className={basicCSS4}
+										data-filter="sprinkle"
+									>
 										Govt. Buildings
 									</button>
-									<button className="btn btn-default filter-button" data-filter="sprinkle">
+									<button
+										onClick={() => {
+											onChange(5);
+										}}
+										className={basicCSS5}
+										data-filter="sprinkle"
+									>
 										Govt. Institutions
 									</button>
-									<button className="btn btn-default filter-button" data-filter="spray">
+									<button
+										onClick={() => {
+											onChange(6);
+										}}
+										className={basicCSS6}
+										data-filter="spray"
+									>
 										Schools
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(7);
+										}}
+										className={basicCSS7}
+										data-filter="irrigation"
+									>
 										Apartments
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(8);
+										}}
+										className={basicCSS8}
+										data-filter="irrigation"
+									>
 										Houses
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(9);
+										}}
+										className={basicCSS9}
+										data-filter="irrigation"
+									>
 										Industries
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(10);
+										}}
+										className={basicCSS10}
+										data-filter="irrigation"
+									>
 										With Dignitires
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(11);
+										}}
+										className={basicCSS11}
+										data-filter="irrigation"
+									>
 										R & D
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(12);
+										}}
+										className={basicCSS12}
+										data-filter="irrigation"
+									>
 										Systems and Solution
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(13);
+										}}
+										className={basicCSS13}
+										data-filter="irrigation"
+									>
 										International Visitors
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(14);
+										}}
+										className={basicCSS14}
+										data-filter="irrigation"
+									>
 										Live-Demo Truck
 									</button>
-									<button className="btn btn-default filter-button" data-filter="irrigation">
+									<button
+										onClick={() => {
+											onChange(15);
+										}}
+										className={basicCSS15}
+										data-filter="irrigation"
+									>
 										IT Sector
 									</button>
 								</div>
@@ -145,23 +392,7 @@ export default function Home() {
 									style={{ marginTop: 40 }}
 									className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"
 								>
-									<Gallery photos={photos} onClick={openLightbox} />
-									<ModalGateway>
-										{viewerIsOpen ? (
-											<Modal onClose={closeLightbox}>
-												<Carousel
-													currentIndex={currentImage}
-													views={photos.map((x) => ({
-														...x,
-														srcset: x.srcSet,
-														caption: x.title
-													}))}
-												/>
-											</Modal>
-										) : null}
-									</ModalGateway>
-
-									{/* <img className=" center-block" src="static/images/gallery/Image1.png" /> */}
+									{gallerySelectedComponent}
 								</div>
 							</div>
 						</div>
