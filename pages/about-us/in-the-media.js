d01a5349fda30d0a155643955e9131a3e781b1bb
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-07-13 15:04:54 +0530

    updated in the media section

--- pages/about-us/in-the-media.js
+++ pages/about-us/in-the-media.js
@@ -1,10 +1,38 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Slider1 from '../../components/SliderNews1';
+import Slider2 from '../../components/SliderNews2';
+import Slider3 from '../../components/SliderNews3';
 
 export default function Home() {
+	const [ currentArticle, setCurrentArticle ] = useState(1);
+
+	const changeToArticle1 = () => {
+		setCurrentArticle(1);
+	};
+
+	const changeToArticle2 = () => {
+		setCurrentArticle(2);
+	};
+	const changeToArticle3 = () => {
+		setCurrentArticle(3);
+	};
+
+	let newsArticle = <Slider1 article2={changeToArticle2} />;
+
+	if (currentArticle === 1) {
+		newsArticle = <Slider1 article2={changeToArticle2} />;
+	}
+	if (currentArticle === 2) {
+		newsArticle = <Slider2 article1={changeToArticle1} article3={changeToArticle3} />;
+	}
+	if (currentArticle === 3) {
+		newsArticle = <Slider3 article2={changeToArticle2} />;
+	}
+
 	return (
 		<Fragment>
 			<Head>
@@ -43,253 +71,7 @@ export default function Home() {
 							<h2 className="team-heading media-hdng">MEDIA COVERAGE & ARTICLES</h2>
 						</div>
 					</div>
-					<Fragment>
-						<div className="container media-main">
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media1.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												<a href="google.com">
-													Farmland Rainwater Harvesting Systems Wins Aqua Foundations
-													Excellence Award. 1
-												</a>
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web.{' '}
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media2.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												SKOCH Award Winner: Farmland Rainwater harvesting Systems.<br />
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web. Lorum ispum is a dummy text for web.
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media3.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												Where Suvarna Jala Fails, Schools Leap ahead.<br />
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web. Lorum ispum is a dummy text for web.
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="container media-main">
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media1.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												Farmland Rainwater Harvesting Systems Wins Aqua Foundations Excellence
-												Award.
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web.{' '}
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media2.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												SKOCH Award Winner: Farmland Rainwater harvesting Systems.<br />
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web. Lorum ispum is a dummy text for web.
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4">
-								<div className="sec-brdr">
-									<div className="mdiaimg">
-										<img src="../../static/images/media/media3.png" />
-									</div>
-									<div className="mdiacont">
-										<div className="grndtext">
-											<h3>
-												Where Suvarna Jala Fails, Schools Leap ahead.<br />
-											</h3>
-											<p
-												style={{
-													textAlign: 'left',
-													color: '#4c3f43',
-													fontSize: '12px',
-													fontWeight: '500',
-													letterSpacing: '0.5px'
-												}}
-											>
-												Lorum ispum is a dummy text for web. Lorum ispum is a dummy text for
-												web. Lorum ispum is a dummy text for web. Lorum ispum is a dummy text
-												for web. Lorum ispum is a dummy text for web.
-											</p>
-											<div className="mdiasocial">
-												<div className="col-md-6 mediasocial">
-													<i className="fa fa-facebook" aria-hidden="true" />
-													<i className="fa fa-twitter" aria-hidden="true" />
-													<i className="fa fa-google-plus" aria-hidden="true" />
-												</div>
-												<div className="col-md-6 mediabtn">
-													<button>
-														Read more <i class="fa fa-angle-right" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</Fragment>
+					{newsArticle}
 
 					<div className="container-fluid award">
 						<h1>OUR AWARDS</h1>
