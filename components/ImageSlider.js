4100241bc14365bde3fd96a81fbbbec100734146
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-07-20 15:02:30 +0530

    updated with images in the media section

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,17 +1,37 @@
-import React, { Component, Fragment } from 'react';
-import Slider from 'react-slick';
+import React, {Component, Fragment} from "react";
+import Slider from "react-slick";
 
 function SampleNextArrow(props) {
-	const { className, style, onClick } = props;
+	const {className, style, onClick} = props;
 	return (
-		<div className={className} style={{ ...style, display: 'block', background: '#0971b7', padding: '10px 30px 30px 10px', borderRadius: '30px' }} onClick={onClick} />
+		<div
+			className={className}
+			style={{
+				...style,
+				display: "block",
+				background: "#0971b7",
+				padding: "10px 30px 30px 10px",
+				borderRadius: "30px",
+			}}
+			onClick={onClick}
+		/>
 	);
 }
 
 function SamplePrevArrow(props) {
-	const { className, style, onClick } = props;
+	const {className, style, onClick} = props;
 	return (
-		<div className={className} style={{ ...style, display: 'block', background: '#0971b7', padding: '10px 30px 30px 10px', borderRadius: '30px' }} onClick={onClick} />
+		<div
+			className={className}
+			style={{
+				...style,
+				display: "block",
+				background: "#0971b7",
+				padding: "10px 30px 30px 10px",
+				borderRadius: "30px",
+			}}
+			onClick={onClick}
+		/>
 	);
 }
 
@@ -23,7 +43,7 @@ export default class CustomArrows extends Component {
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			nextArrow: <SampleNextArrow />,
-			prevArrow: <SamplePrevArrow />
+			prevArrow: <SamplePrevArrow />,
 		};
 		return (
 			<Fragment>
@@ -34,30 +54,93 @@ export default class CustomArrows extends Component {
 					<Slider {...settings}>
 						<div className="container-fluid award-image">
 							<div className="container awrdimgsec text-center">
-								<img className="rounded mx-auto d-block" src="../../static/images/media/Award1.png" />
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/Aqua-Foundation-2017.jpg"
+								/>
 								<div className="awrdimgcont">
 									<h3>
-										SKOCH ORDER of Merit-<br />Top 100 SME's in INDIA 2018
+										In the year 2017 honoured with National award by Aqua
+										Foundations as Aqua Foundation Excellence Award 2017 under
+										the category of Industrial Excellence in development of
+										Technology in the Field of Rainwater Harvesting
 									</h3>
 								</div>
 							</div>
 						</div>
 						<div className="container-fluid award-image">
 							<div className="container awrdimgsec ">
-								<img className="rounded mx-auto d-block" src="../../static/images/media/Award1.png" />
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/Earth-care-2014.jpg"
+								/>
 								<div className="awrdimgcont">
 									<h3>
-										2 SKOCH ORDER of Merit-<br />Top 100 SME's in INDIA 2018
+										In the year 2014 International award from JSW – The Times of
+										India “Earth Care Awarded for the Innovative FL-V Wire
+										Injection Well Technology in the Category of `Innovation for
+										Climate Protection’.
 									</h3>
 								</div>
 							</div>
 						</div>
 						<div className="container-fluid award-image">
 							<div className="container awrdimgsec">
-								<img className="rounded mx-auto d-block" src="../../static/images/media/Award1.png" />
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/Earthcare-2010.jpg"
+								/>
 								<div className="awrdimgcont">
 									<h3>
-										3 SKOCH ORDER of Merit-<br />Top 100 SME's in INDIA 2018
+										in the year 2010 International Award from JSW – The Times of
+										India as “Earth Care Award for Innovation for climate
+										protection” for the Invention of `Rainy’ - Self-cleaning
+										Auto Flush Out Filters.
+									</h3>
+								</div>
+							</div>
+						</div>
+						<div className="container-fluid award-image">
+							<div className="container awrdimgsec">
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/National-Award--by-CII--2009.jpg"
+								/>
+								<div className="awrdimgcont">
+									<h3>
+										National Award for Excellence in Water Management (2009),
+										under -the most Innovative Water Saving Product (Rainy
+										Filters), by Confederation of Indian Industry (CII).
+									</h3>
+								</div>
+							</div>
+						</div>
+						<div className="container-fluid award-image">
+							<div className="container awrdimgsec">
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/SKOCH--order-of-merit-among-100-SMES--2018.jpg"
+								/>
+								<div className="awrdimgcont">
+									<h3>
+										In the year 2018 SKOCH Group-New Delhi awarded `Rainy
+										‘Filters - SKOCH ORDER of –Merit for qualifying amongst
+										TOP-100 SMES in India
+									</h3>
+								</div>
+							</div>
+						</div>
+						<div className="container-fluid award-image">
+							<div className="container awrdimgsec">
+								<img
+									className="rounded mx-auto d-block"
+									src="../../static/images/media/SKOCH-award-silver-achiever---innovation-2018.jpg"
+								/>
+								<div className="awrdimgcont">
+									<h3>
+										In 2018 Conferred Highest Independent Honour by honouring
+										SKOCH AWARD –ACHIEVER Silver for The Innovation of `Rainy’
+										Filters.
 									</h3>
 								</div>
 							</div>
