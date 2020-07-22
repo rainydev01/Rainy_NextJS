7edbe17436ea745855f14feb4a1fe34b1c9da880
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-07-22 12:38:43 +0530

    CSS changes

--- components/OurClients.js
+++ components/OurClients.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
+import React, {Component} from "react";
+import Slider from "react-slick";
 
 function SampleNextArrow(props) {
-	const { className, style, onClick } = props;
+	const {className, style, onClick} = props;
 	return (
-		<div className={className} style={{ ...style, display: 'block', background: '#0971b7' }} onClick={onClick} />
+		<div
+			className={className}
+			style={{...style, display: "block", background: "#0971b7"}}
+			onClick={onClick}
+		/>
 	);
 }
 
 function SamplePrevArrow(props) {
-	const { className, style, onClick } = props;
+	const {className, style, onClick} = props;
 	return (
-		<div className={className} style={{ ...style, display: 'block', background: '#0971b7' }} onClick={onClick} />
+		<div
+			className={className}
+			style={{...style, display: "block", background: "#0971b7"}}
+			onClick={onClick}
+		/>
 	);
 }
 
@@ -23,10 +31,24 @@ export default class CustomArrows extends Component {
 			slidesToShow: 5,
 			slidesToScroll: 1,
 			nextArrow: <SampleNextArrow />,
-			prevArrow: <SamplePrevArrow />
+			prevArrow: <SamplePrevArrow />,
+
+			responsive: [
+				{
+					breakpoint: 600,
+					settings: {
+						dots: false,
+						infinite: false,
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						nextArrow: <SampleNextArrow />,
+						prevArrow: <SamplePrevArrow />,
+					},
+				},
+			],
 		};
 		return (
-			<div style={{ padding: 10, margin: 40 }}>
+			<div style={{padding: 10, margin: 40}}>
 				<div className="container-fluid">
 					<div className="container">
 						<h2 className="clientsHome">Our Clients</h2>
@@ -34,40 +56,88 @@ export default class CustomArrows extends Component {
 				</div>
 				<Slider {...settings}>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/BHEL.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/BHEL.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/BWSSB.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/BWSSB.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/Capture.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/Capture.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/Creamline.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/Creamline.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/IFFCO.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/IFFCO.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/infosys-logo-PNG.png" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/infosys-logo-PNG.png"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/IOCL.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/IOCL.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/JSW.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/JSW.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/Konkan Railways.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/Konkan Railways.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/KUIDFC.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/KUIDFC.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/Mahindra.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/Mahindra.PNG"
+						/>
 					</div>
 					<div className="col-md-2">
-						<img height="90" width="200" src="../static/images/clients/Wipro.PNG" />
+						<img
+							height="90"
+							width="200"
+							src="../static/images/clients/Wipro.PNG"
+						/>
 					</div>
 				</Slider>
 			</div>
