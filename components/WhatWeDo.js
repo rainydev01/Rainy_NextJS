d992189657e9a47f28f252738bd6967e860e91be
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-07-27 14:06:11 +0530

    css updates for font

--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from "react";
 
 const WhatWeDo = () => {
 	return (
@@ -13,7 +13,7 @@ const WhatWeDo = () => {
 				<div className="container">
 					<div className="">
 						<div className="col-md-4">
-							<div style={{ textAlign: 'right' }}>
+							<div style={{textAlign: "right"}}>
 								<img alt="logo" src="../static/images/homepage/whatWeDo1.png" />
 							</div>
 						</div>
@@ -22,41 +22,47 @@ const WhatWeDo = () => {
 								<h3>Groundwater Recharging</h3>
 								<p
 									style={{
-										letterSpacing: '0.1px',
-										textAlign: 'left',
-										color: '#4c3f43',
-										fontSize: '14px',
-										fontWeight: '500',
-										lineHeight: '26px'
+										letterSpacing: "0.1px",
+										textAlign: "left",
+										color: "#4c3f43",
+										fontSize: "16px",
+										fontWeight: "500",
+										lineHeight: "26px",
+										fontFamily: "Visby Round CF Regular",
 									}}
 								>
-									We have developed a new method of groundwater recharge, called 'V-wire injection
-									well' technology. This allows for the ground water table to be replenished and thus
-									abate water scarcity.
+									We have developed a new method of groundwater recharge, called
+									'V-wire injection well' technology. This allows for the ground
+									water table to be replenished and thus abate water scarcity.
 								</p>
 							</div>
 						</div>
 						<div className="col-md-4">
 							<div>
-								<img alt="iamegs" src="../static/images/homepage/whatWeDo2.png" />
+								<img
+									alt="iamegs"
+									src="../static/images/homepage/whatWeDo2.png"
+								/>
 							</div>
 						</div>
 						<div className="col-md-4">
 							<div className="grndtext">
-								<h3 style={{ textAlign: 'right' }}>Rooftop RWH Filters</h3>
+								<h3 style={{textAlign: "right"}}>Rooftop RWH Filters</h3>
 								<p
 									style={{
-										letterSpacing: '0.1px',
-										textAlign: 'right',
-										color: '#4c3f43',
-										fontSize: '14px',
-										fontWeight: '500',
-										lineHeight: '26px'
+										letterSpacing: "0.1px",
+										textAlign: "right",
+										color: "#4c3f43",
+										fontSize: "16px",
+										fontWeight: "500",
+										lineHeight: "26px",
+										fontFamily: "Visby Round CF Regular",
 									}}
 								>
-									Rainy Dual Intensity filters work at high efficiency regardless of the intensity of
-									rainfall and have self-cleaning abilities. At nominal prices, our filters are market
-									leaders in Rooftop RWH.
+									Rainy Dual Intensity filters work at high efficiency
+									regardless of the intensity of rainfall and have self-cleaning
+									abilities. At nominal prices, our filters are market leaders
+									in Rooftop RWH.
 								</p>
 							</div>
 						</div>
@@ -70,16 +76,18 @@ const WhatWeDo = () => {
 								<h3>Systems & Solutions</h3>
 								<p
 									style={{
-										letterSpacing: '0.1px',
-										textAlign: 'left',
-										color: '#4c3f43',
-										fontSize: '14px',
-										fontWeight: '500',
-										lineHeight: '26px'
+										letterSpacing: "0.1px",
+										textAlign: "left",
+										color: "#4c3f43",
+										fontSize: "16px",
+										fontWeight: "500",
+										lineHeight: "26px",
+										fontFamily: "Visby Round CF Regular",
 									}}
 								>
-									Custom made solutions to suit your building or campus. Our expert engineers will
-									help from design to execution of end to end RWH solutions for your facility.
+									Custom made solutions to suit your building or campus. Our
+									expert engineers will help from design to execution of end to
+									end RWH solutions for your facility.
 								</p>
 							</div>
 						</div>
