1a97f689c282382845cd2e5cd73d7da909e907de
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-12-04 00:53:11 +0530

    updated with google translate in footer

--- pages/index.js
+++ pages/index.js
@@ -44,10 +44,6 @@ export default function Home() {
 						},
 					}}
 				/>
-				{/* <script
-					type="text/javascript"
-					src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-				></script> */}
 
 				<title>Rainy - RWH Systems</title>
 				<link
