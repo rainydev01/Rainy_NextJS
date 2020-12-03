38ba13fb2997f94142138399e16e019244c58f1f
ChiragRamachandra <chirag.ramachandra@gmail.com>
2020-12-04 01:05:43 +0530

    updated with gtranslate

--- components/FooterDealer.js
+++ components/FooterDealer.js
@@ -84,14 +84,6 @@ const Footer = () => {
 										<a href="/contact-us/enquire">Enquire</a>{" "}
 									</button>
 								</div>
-
-								{/* <div id="google_translate_element"></div> */}
-
-								<script
-									type="text/javascript"
-									src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-								/>
-								<div id="google_translate_element"></div>
 							</div>
 						</div>
 					</div>
