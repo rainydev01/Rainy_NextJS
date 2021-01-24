319dbee62c4883696f83fd0b054a285a5576577f
ChiragRamachandra <chirag.ramachandra@gmail.com>
2021-01-24 14:02:04 +0530

    updated with removing google translate options

--- pages/about-us/in-the-media.js
+++ pages/about-us/in-the-media.js
@@ -1,42 +1,42 @@
-import {Fragment, useState} from "react";
-import Head from "next/head";
-import Link from "next/link";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import Slider1 from "../../components/SliderNews1";
-import Slider2 from "../../components/SliderNews2";
-import Slider3 from "../../components/SliderNews3";
+import {Fragment, useState} from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Slider1 from '../../components/SliderNews1'
+import Slider2 from '../../components/SliderNews2'
+import Slider3 from '../../components/SliderNews3'
 // import ImageSlider from "../../components/ImageSlider";
-import ImageSlider1 from "../../components/ImageSliderMulti";
+import ImageSlider1 from '../../components/ImageSliderMulti'
 
-import ImageSliderArticles from "../../components/ImageSliderForArticles";
+import ImageSliderArticles from '../../components/ImageSliderForArticles'
 
 export default function Home() {
-	const [currentArticle, setCurrentArticle] = useState(1);
+	const [currentArticle, setCurrentArticle] = useState(1)
 
 	const changeToArticle1 = () => {
-		setCurrentArticle(1);
-	};
+		setCurrentArticle(1)
+	}
 
 	const changeToArticle2 = () => {
-		setCurrentArticle(2);
-	};
+		setCurrentArticle(2)
+	}
 	const changeToArticle3 = () => {
-		setCurrentArticle(3);
-	};
+		setCurrentArticle(3)
+	}
 
-	let newsArticle = <Slider1 article2={changeToArticle2} />;
+	let newsArticle = <Slider1 article2={changeToArticle2} />
 
 	if (currentArticle === 1) {
-		newsArticle = <Slider1 article2={changeToArticle2} />;
+		newsArticle = <Slider1 article2={changeToArticle2} />
 	}
 	if (currentArticle === 2) {
 		newsArticle = (
 			<Slider2 article1={changeToArticle1} article3={changeToArticle3} />
-		);
+		)
 	}
 	if (currentArticle === 3) {
-		newsArticle = <Slider3 article2={changeToArticle2} />;
+		newsArticle = <Slider3 article2={changeToArticle2} />
 	}
 
 	return (
@@ -44,7 +44,7 @@ export default function Home() {
 			<Head>
 				<script
 					async
-					src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1"
+					src='https://www.googletagmanager.com/gtag/js?id=UA-168445645-1'
 				></script>
 
 				<script
@@ -59,21 +59,21 @@ export default function Home() {
 				/>
 				<title>Rainy - Media</title>
 				<link
-					rel="stylesheet"
-					href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-					integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-					crossOrigin="anonymous"
+					rel='stylesheet'
+					href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
+					integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk'
+					crossOrigin='anonymous'
 				/>
 				<link
-					rel="stylesheet"
-					href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+					rel='stylesheet'
+					href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
 				/>
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" />
+				<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js' />
+				<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js' />
+				<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js' />
 				<link
-					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+					rel='stylesheet'
+					href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 				/>
 				<script
 					dangerouslySetInnerHTML={{
@@ -81,10 +81,10 @@ export default function Home() {
 							/* eslint-disable no-new */
 							new window.google.translate.TranslateElement(
 								{
-									pageLanguage: "pt",
+									pageLanguage: 'pt',
 								},
-								"google_translate_element"
-							);
+								'google_translate_element'
+							)
 						},
 					}}
 				/>
@@ -112,22 +112,22 @@ src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
 				/>
 			</Head>
 			<main>
-				<div className="App">
+				<div className='App'>
 					{/*<h1> Media</h1>*/}
 					<Header />
 
-					<div className="container-fluid banner enternal">
-						<div className="hero-image enthroimg">
-							<img src="../../static/images/media/mediaBanner.jpg" />
-							<div className="hero-text inthemedia">
-								<h1 style={{textAlign: "center"}}>IN THE MEDIA</h1>
+					<div className='container-fluid banner enternal'>
+						<div className='hero-image enthroimg'>
+							<img src='../../static/images/media/mediaBanner.jpg' />
+							<div className='hero-text inthemedia'>
+								<h1 style={{textAlign: 'center'}}>IN THE MEDIA</h1>
 							</div>
 						</div>
 					</div>
-					<div className="container" />
-					<div className="container">
-						<div className="groundwater_bg team blue_underline">
-							<h2 className="team-heading media-hdng blue_underline">
+					<div className='container' />
+					<div className='container'>
+						<div className='groundwater_bg team blue_underline'>
+							<h2 className='team-heading media-hdng blue_underline'>
 								MEDIA COVERAGE & ARTICLES
 							</h2>
 						</div>
@@ -144,5 +144,5 @@ src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
 
 			<footer />
 		</Fragment>
-	);
+	)
 }
