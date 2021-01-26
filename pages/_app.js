cd3c64fed0ae406cb92046e939ef4faf349f5288
ChiragRamachandra <chirag.ramachandra@gmail.com>
2021-01-27 01:34:59 +0530

    updated with ios landscape fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
-import "../assets/css/style.css";
-import "../assets/css/style2.css";
-import "../assets/css/style3.css";
-import "../assets/css/safari-css/i-pad/V11.css";
-import "../assets/css/safari-css/i-pad/V12.css";
-import "../assets/css/safari-css/i-pad/V14.css";
-import "../assets/css/safari-css/phone/V11.css";
-import "../assets/css/safari-css/phone/V12.css";
-import "../assets/css/safari-css/phone/V14.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "react-multi-carousel/lib/styles.css";
+import '../assets/css/style.css'
+import '../assets/css/style2.css'
+import '../assets/css/style3.css'
+import '../assets/css/header.css'
+
+// import "../assets/css/safari-css/i-pad/V11.css";
+// import "../assets/css/safari-css/i-pad/V12.css";
+// import "../assets/css/safari-css/i-pad/V14.css";
+// import "../assets/css/safari-css/phone/V11.css";
+// import "../assets/css/safari-css/phone/V12.css";
+// import "../assets/css/safari-css/phone/V14.css";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import 'react-multi-carousel/lib/styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return <Component {...pageProps} />
 }
