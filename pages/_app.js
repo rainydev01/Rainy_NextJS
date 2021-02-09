f67ae0e9c2f6d9b249e8d45c5000691122d955e4
ChiragRamachandra <chirag.ramachandra@gmail.com>
2021-02-09 16:43:58 +0530

    updated with banner fixes

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../assets/css/style.css'
 import '../assets/css/style2.css'
 import '../assets/css/style3.css'
 import '../assets/css/header.css'
+import '../assets/css/alignment.css'
 
 // import "../assets/css/safari-css/i-pad/V11.css";
 // import "../assets/css/safari-css/i-pad/V12.css";
