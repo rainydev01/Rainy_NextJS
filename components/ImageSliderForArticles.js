03a7744e37958f849340db95d200c3c7bc02f098
Aravindlnc <87873491+Aravindlnc@users.noreply.github.com>
2022-10-01 09:37:11 +0530

    new blog added

--- components/ImageSliderForArticles.js
+++ components/ImageSliderForArticles.js
@@ -103,7 +103,78 @@ const ImageSliderMulti = () => {
 								</div>
 							</div>
 						</div>
+<div className='col-md-4'>
+							<div className='sec-brdr'>
+								<div className='mdiaimg'>
+									<img src='../static/images/media/articles/Benefits_of_Rainwater_Harvesting.jpg' />
+								</div>
+								<div className='mdiacont'>
+									<div className='grndtext'>
+										<h3>
+											<a
+												href='https://medium.com/@aravie12121/the-benefits-of-rain-water-harvesting-why-you-should-start-collecting-rain-water-281609160484'
+												target='_blank'
+											>
+												Karnataka: Water research station to come in Chikmagalur
+												<br />
+											</a>
+										</h3>
+										<p
+											style={{
+												textAlign: 'left',
+												color: '#4c3f43',
+												fontSize: '13px',
+												fontWeight: '500',
+												letterSpacing: '0.5px',
+											}}
+										>
+											Self-funded, and probably one-of-its-kind in India, the
+											project will have state of the art hydrology labs and
+											rainwater harvesting models.
+										</p>
+										<div className='col-md-12 mdiasocial'>
+											<div className='col-md-6 mediasocial'>
+												<FacebookShareButton
+													url={
+														'https://medium.com/@aravie12121/the-benefits-of-rain-water-harvesting-why-you-should-start-collecting-rain-water-281609160484'
+													}
+												>
+													<i className='fa fa-facebook' aria-hidden='true' />
+												</FacebookShareButton>
 
+												<TwitterShareButton
+													url={
+														'https://medium.com/@aravie12121/the-benefits-of-rain-water-harvesting-why-you-should-start-collecting-rain-water-281609160484'
+													}
+												>
+													<i className='fa fa-twitter' aria-hidden='true' />
+												</TwitterShareButton>
+
+												<WhatsappShareButton
+													url={
+														'https://medium.com/@aravie12121/the-benefits-of-rain-water-harvesting-why-you-should-start-collecting-rain-water-281609160484'
+													}
+												>
+													<i className='fa fa-whatsapp' aria-hidden='true' />
+												</WhatsappShareButton>
+											</div>
+											<div className='col-md-6 mediabtn'>
+												<button>
+													<a
+														href='https://medium.com/@aravie12121/the-benefits-of-rain-water-harvesting-why-you-should-start-collecting-rain-water-281609160484'
+														target='_blank'
+													>
+														Read more
+													</a>
+													{'  '}
+													<i class='fa fa-angle-right' aria-hidden='true' />
+												</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 						<div className='col-md-4'>
 							<div className='sec-brdr'>
 								<div className='mdiaimg'>
