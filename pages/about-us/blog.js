a26f1ea0fbc88b12da18b51936d485836f2e2af4
Aravindlnc <87873491+Aravindlnc@users.noreply.github.com>
2023-02-25 20:01:30 +0530

    Update blog.js
    added title and descriptions

--- pages/about-us/blog.js
+++ pages/about-us/blog.js
@@ -40,6 +40,8 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
+	  <title>Blogs about rain water rainwater harvesting filters, benefits and usages, advantages, more</title>
+<meta name="description" content="Get all the information you need to know about rain water harvesting and how it can benefit you. We have a wide range of articles discussing the advantages and disadvantages of rain water harvesting, as well as tips on how to get started. Check us out today to see how rain water harvesting can change your life."/>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1"
