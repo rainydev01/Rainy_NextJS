6f0693c13d6149c6cfdb20f23a08d9b5d7fa80b4
dranjangowda <dranjanjgowda@gmail.com>
2023-03-18 13:38:48 +0530

    index page error solved

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,122 @@
-import {Fragment} from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
-import WhatWeDo from '../components/WhatWeDo'
-import Footer from '../components/Footer'
-import AboutUs from '../components/AboutUs'
-import RWHFilters from '../components/RWHFilters'
-import FilterToSelect from '../components/FilterToSelect'
-import GroundWaterRecharging from '../components/GroundWaterRecharging'
-import OurClients from '../components/OurClientsMulti'
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import WhatWeDo from "../components/WhatWeDo";
+import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
+import RWHFilters from "../components/RWHFilters";
+import FilterToSelect from "../components/FilterToSelect";
+import GroundWaterRecharging from "../components/GroundWaterRecharging";
+import OurClients from "../components/OurClientsMulti";
 
 export default function Home() {
-	return (
-		<Fragment>
-			<Head>
-				<script
-					async
-					src='https://www.googletagmanager.com/gtag/js?id=UA-168445645-1'
-				></script>
+  return (
+    <Fragment>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1"
+        ></script>
 
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-														  window.dataLayer = window.dataLayer || [];
-  														  function gtag(){dataLayer.push(arguments);}
-  														  gtag('js', new Date());
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+														  window.dataLayer = window.dataLayer || [];
+  														  function gtag(){dataLayer.push(arguments);}
+  														  gtag('js', new Date());
 															gtag('config', 'UA-168445645-1');
-        `,
-					}}
-				/>
+        `,
+          }}
+        />
 
-				<title>Rainyfilters - Rainwater harvesting system in India | Company | Consultants | Services</title>
+        <title>
+          Rainyfilters - Rainwater harvesting system in India | Company |
+          Consultants | Services
+        </title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="Are you looking for a rain water harvesting system in India? We are a rainwater harvesting company that provides rain water harvesting consultants and services. We can help you install a rainwater harvesting system in your home or business. Contact us today for a free consultation."
+        />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossOrigin="anonymous"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: function googleTranslateElementInit() {
+              /* eslint-disable no-new */
+              new window.google.translate.TranslateElement(
+                {
+                  pageLanguage: "pt",
+                },
+                "google_translate_element"
+              );
+            },
+          }}
+        />
 
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://www.rainyfilters.com/">
-<meta property="og:title" content="Rainyfilters - Rainwater harvesting system in India | Company | Consultants | Services">
-<meta property="og:description" content="Are you looking for a rain water harvesting system in India? We are a rainwater harvesting company that provides rain water harvesting consultants and services. We can help you install a rainwater harvesting system in your home or business. Contact us today for a free consultation.">
-<meta property="og:image" content="https://www.rainyfilters.com/images/logo/Logo_Main.png">
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Farmland Rainwater Harvesting systems",
+              alternateName: "Rainy",
+              url: "http://rainyfilters.com/",
+              logo: "",
+              sameAs: [
+                " https://www.facebook.com/Rainyfilters2002",
+                "https://twitter.com/rainy_filters02",
+                "https://www.instagram.com/rainy_filters02/",
+                "https://www.youtube.com/channel/UCmGOEYld3MLeJJ3KtZt0NzA",
+                "https://www.linkedin.com/company/rainy",
+                "https://www.indiamart.com/farmlandrainwaterharvestingsystems/",
+                "http://www.rainyfilters.com/",
+              ],
+            }),
+          }}
+        />
 
-
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://www.rainyfilters.com/">
-<meta property="twitter:title" content="Rainyfilters - Rainwater harvesting system in India | Company | Consultants | Services">
-<meta property="twitter:description" content="Are you looking for a rain water harvesting system in India? We are a rainwater harvesting company that provides rain water harvesting consultants and services. We can help you install a rainwater harvesting system in your home or business. Contact us today for a free consultation.">
-<meta property="twitter:image" content="https://www.rainyfilters.com/images/logo/Logo_Main.png">
-				
-				<link
-					rel='apple-touch-icon'
-					sizes='180x180'
-					href='/apple-touch-icon.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='/favicon-32x32.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='16x16'
-					href='/favicon-16x16.png'
-				/>
-				<link rel='manifest' href='/site.webmanifest' />
-				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
-				<meta name='msapplication-TileColor' content='#2b5797' />
-				<meta name='theme-color' content='#ffffff' />
-				<meta name="description" content="Are you looking for a rain water harvesting system in India? We are a rainwater harvesting company that provides rain water harvesting consultants and services. We can help you install a rainwater harvesting system in your home or business. Contact us today for a free consultation." />	
-				<link rel="canonical" href=" https://www.rainyfilters.com/" />				
-					<link
-					rel='stylesheet'
-					href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
-				/>
-				<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-				<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js'></script>
-				<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'></script>
-				<link
-					rel='stylesheet'
-					href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-				/>
-				<link
-					rel='stylesheet'
-					href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
-					integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk'
-					crossOrigin='anonymous'
-				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: function googleTranslateElementInit() {
-							/* eslint-disable no-new */
-							new window.google.translate.TranslateElement(
-								{
-									pageLanguage: 'pt',
-								},
-								'google_translate_element'
-							)
-						},
-					}}
-				/>
-
-				<script
-					type='application/ld+json'
-					dangerouslySetInnerHTML={{
-						__html: JSON.stringify({
-							'@context': 'https://schema.org',
-							'@type': 'Organization',
-							name: 'Farmland Rainwater Harvesting systems',
-							alternateName: 'Rainy',
-							url: 'http://rainyfilters.com/',
-							logo: '',
-							sameAs: [
-								' https://www.facebook.com/Rainyfilters2002',
-								'https://twitter.com/rainy_filters02',
-								'https://www.instagram.com/rainy_filters02/',
-								'https://www.youtube.com/channel/UCmGOEYld3MLeJJ3KtZt0NzA',
-								'https://www.linkedin.com/company/rainy',
-								'https://www.indiamart.com/farmlandrainwaterharvestingsystems/',
-								'http://www.rainyfilters.com/',
-							],
-						}),
-					}}
-				/>
-
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -136,31 +127,31 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '587685655184685');
 fbq('track', 'PageView');`,
-					}}
-				/>
-				<noscript
-					dangerouslySetInnerHTML={{
-						__html: `<img height="1" width="1" style="display:none"
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
 />`,
-					}}
-				/>
-			</Head>
-			<main>
-				<div className='App'>
-					<Header />
-					<Banner />
-					<WhatWeDo />
-					<AboutUs />
-					<RWHFilters />
-					<FilterToSelect />
-					<GroundWaterRecharging />
-					<OurClients />
-					<Footer />
-				</div>
-			</main>
+          }}
+        />
+      </Head>
+      <main>
+        <div className="App">
+          <Header />
+          <Banner />
+          <WhatWeDo />
+          <AboutUs />
+          <RWHFilters />
+          <FilterToSelect />
+          <GroundWaterRecharging />
+          <OurClients />
+          <Footer />
+        </div>
+      </main>
 
-			<footer />
-		</Fragment>
-	)
+      <footer />
+    </Fragment>
+  );
 }
