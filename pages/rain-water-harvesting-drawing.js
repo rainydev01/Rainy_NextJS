ab8a558d428688fa6c93762aff8fe6dacd69c3e8
Prashant <Khanayatprashant@gmail.com>
2023-08-22 00:29:18 +0530

    add a blog and add new page

---
+++ pages/rain-water-harvesting-drawing.js
@@ -0,0 +1,258 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+														  window.dataLayer = window.dataLayer || [];
+  														  function gtag(){dataLayer.push(arguments);}
+  														  gtag('js', new Date());
+															gtag('config', 'UA-168445645-1');
+        `,
+          }}
+        />
+
+        <title>
+        Rain Water Harvesting Drawing | Rainyfilters.com
+        </title>
+        <link rel="canonical" href="https://www.rainyfilters.com/technology" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossOrigin="anonymous"
+        />
+        <meta name="description" content="If you're looking for a rainwater harvesting drawing that you don't see here, chances are that we have one available to send to you. Visit our official website www.rainyfilters.com"/>
+          <meta name="keywords" content="rain water harvesting drawing, Simple rain water harvesting drawing, Rain water harvesting drawing step by step" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Open+Sans&family=Raleway&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: function googleTranslateElementInit() {
+              /* eslint-disable no-new */
+              new window.google.translate.TranslateElement(
+                {
+                  pageLanguage: "pt",
+                },
+                "google_translate_element"
+              );
+            },
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '587685655184685');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
+/>`,
+          }}
+        />
+      </Head>
+      <main>
+        <div className="App">
+          <Header />
+
+          <div className="container-fluid banner enternal">
+            <div className="hero-image enthroimg">
+              {/*<img src="../static/images/technology/ourInnovativeTechnologyBanner.png" />*/}
+              {/* <div className='hero-text techbnnrtxt'>
+								<h1 style={{color: 'white'}}>
+									OUR INNOVATIVE
+									<br />
+									TECHNOLOGY
+								</h1>
+							</div> */}
+            </div>
+          </div>
+
+          <div className="container-fluid techbgmain">
+            <div className="container">
+              <div className="techhedng">
+                <h2 className="tech-heading">Rainwater Harvesting Drawing </h2>
+                <p className="techpara">
+                TheRainwater harvesting drawings are like blueprints before building something. They help plan how to collect rainwater in the best way. With less water available and environmental problems getting worse, using rainwater smartly is a good idea. Rainwater harvesting is like a clever solution to make the most out of the water we get from rain. It helps us plan ahead and make sure we're using water well.
+                </p>
+                <p>As our world is facing problems like not having enough water and taking care of our environment, rainwater harvesting is like a smart solution. These drawings help us plan ahead, so when it rains, we can catch the water and use it for different things like watering plants or cleaning.</p>
+                <p>So, rainwater harvesting drawings are like guides that show us where to put things to catch and use rainwater in a really smart and effective way.</p>
+              </div>
+
+              <div className="techhedng">
+                <h3 className="tech-heading">Explanation </h3>
+                <p className="techpara">
+                Before putting a rainwater harvesting system in place, careful planning and thoughtful design are crucial. Think of it as crafting a precise strategy. The process involves creating detailed <a href="https://www.rainyfilters.com/technology">rainwater harvesting drawings</a> that map out the entire system. These drawings ensure that the system fits the specific needs and conditions of the location. To put it in perspective, imagine building a complex structure. You wouldn't start without a detailed blueprint, right? Similarly, rainwater harvesting drawings act as these blueprints. They help us figure out where everything goes in the rainwater system before we start putting it together.
+                </p>
+                <p>When we make these drawings, we need to consider many factors. We analyze how much rain falls, where it lands, how much water we want to save, and how to make sure the water is clean and safe. All these details are essential for a successful system. The collected rainwater serves a variety of purposes, from watering plants to flushing toilets, doing laundry, and even being used by industries. With proper treatment and filtering, it can even become suitable for drinking and cooking. This becomes particularly valuable in areas where water is scarce or not very clean.</p>
+                <p> To assist in creating these plans, engineers, architects, and designers have access to tools like AutoCAD. This software is like a specialized tool that helps them create detailed drawings of the rainwater harvesting system components. This ensures meticulous planning and accurate execution. So, rainwater harvesting drawings are like detailed maps that guide the creation of an effective rainwater collection system. They play a vital role in meticulous planning, enabling us to use rainwater in a smart, strategic, and sustainable manner, especially in regions facing water challenges.</p>
+              </div>
+
+              <div className="techhedng">
+                  <p>Here are a few designs that we can do using Auto CAD in the field of rainwater harvesting:</p>
+                  <ul>
+                      <li>
+                          <p><strong>1.	Designing Where Rainwater Collects:</strong> With AutoCAD, we can create accurate pictures of places where rainwater gathers, like roofs, paved areas, and open spaces.
+                          </p>
+                          <div className="blogImg hero-image enthroimg">
+                                <img src="../../../static/images/rain-water-harvesting-drawing/inner-1.png"></img>
+                          </div>
+                      </li>
+
+                      <li>
+                        <p><strong>2.	Planning the Gutters and Pipes: </strong> We need the right channels to guide rainwater from where it collects to storage tanks. AutoCAD helps make detailed plans for gutters and pipes, considering their size, shape, and materials.</p>
+                      </li>
+
+                      <li>
+                          <p><strong>3.	Creating Storage Tanks: </strong> AutoCAD helps us design tanks to store rainwater. We consider how much it rains, the area where we collect water, and how much water we need. We can make 3D models of the tanks to fit them well and collect as much rainwater as possible.</p>
+                          <div className="blogImg hero-image enthroimg">
+                                <img src="../../../static/images/rain-water-harvesting-drawing/inner-2.png"></img>
+                          </div>                          
+                      </li>
+
+                      <li>
+                          <p><strong>4.	Building Drains: </strong> Drains are important to avoid too much water collecting in one place. AutoCAD helps design these pathways so that rainwater goes to the storage tanks without flooding.</p>
+                      </li>
+
+                      <li>
+                          <p><strong>5.	Adding Filters: </strong> Sometimes, rainwater can have dirt and debris. AutoCAD helps us design systems that clean the water before we use it.</p>
+                          <div className="blogImg hero-image enthroimg">
+                                <img src="../../../static/images/rain-water-harvesting-drawing/inner-3.png"></img>
+                          </div>
+                      </li>
+
+                      <li>
+                          <p><strong>6.	Seeing the Whole Plan: </strong> AutoCAD can show us how the whole rainwater system will look in 2D and 3D. This helps us see if everything fits well, works properly, and fits with the environment.</p>
+                      </li>
+
+                      <li>
+                          <p><strong>7.	Checking How Water Moves: </strong> Using AutoCAD, we can simulate and see how rainwater will flow through the system during different weather. This helps us make sure everything works well and there are no issues like too much water overflowing.</p>
+                      </li>
+                  </ul>
+
+                  <p> So, AutoCAD helps designers create accurate plans for different parts of the rainwater system, making sure everything works together smoothly and efficiently.</p>
+
+                  <p>Remember, the choice of software depends on your familiarity with the tools and the complexity of the drawings you want to create.</p>
+
+                  <p>Here are the other software tools that can be used for creating rainwater harvesting drawings besides AutoCAD. Some of these include:</p>
+
+                  <ul>
+                      <li>
+                          <p><strong>1.	Solid Edge: </strong> Solid Edge is a software tool used to design rainwater harvesting systems. It helps engineers and designers create detailed drawings and 3D models of these systems, ensuring efficient collection and utilization of rainwater for various purposes. With Solid Edge, you can visualize, plan, and optimize rainwater harvesting setups for sustainable water management.</p>
+                      </li>
+
+                      <li>
+                          <p><strong>2.	Revit: </strong> Revit is a Building Information Modelling (BIM) software that architects and engineers often use. It’s great for creating detailed designs including rain water harvesting system.</p>                          
+                      </li>
+
+                      <li>
+                          <p><strong>3.	Solid Works: </strong> SolidWorks is a powerful design software often used to create rainwater harvesting systems. It helps designers create detailed drawings of the system's components and layout, ensuring accurate representation and efficient implementation. With SolidWorks, designing rainwater harvesting systems becomes easier and more precise.</p>
+
+                      </li>
+                  </ul>
+              </div>
+
+              <div className="techhedng">
+                  <h4>Conclusion</h4>
+
+                  <p>As we aim for a better future that takes care of our planet, rainwater harvesting is a smart and nature-friendly way to help. It lets communities play a part in protecting one of the most important things we have: water.</p>
+
+                  <p>By keeping people informed, using these methods, and finding new ways to draw plans for rainwater systems, we can all work together. This way, we can make sure we have enough water and also keep our planet healthy. This effort will make sure that even the generations that come after us will have enough water and a world that's friendly to nature.</p>
+              </div>
+
+              <div className="techhedng">
+                  <h3>FAQ (Frequently Asked Questions)</h3>
+
+                  <div>
+                      <p>1.	Why are rainwater harvesting drawings necessary?</p>
+                      <p><strong>Ans:</strong> Rainwater harvesting drawings are really important because they help us plan and visualize how rainwater systems will work. They show us how everything will be set up and how the system fit together, which makes it easier to actually build and use these systems.</p>
+                  </div>
+
+                  <div>
+                      <p>2.	Can I create rainwater harvesting drawings without professional help?</p>
+                      <p><strong>Ans:</strong> While professional assistance can be valuable, even if you're not an expert, you can still make rainwater harvesting drawings. If you know a bit about how the system works and the main ideas behind designing it, you can create these drawings yourself.</p>
+                  </div>
+
+                  <div>
+                      <p>3.	What software can I use to create rainwater harvesting designs?</p>
+                      <p><strong>Ans:</strong> Various computer-aided software programs, such as AutoCAD or SketchUp,you can use it to make very accurate and detailed drawings for collecting rainwater.</p>
+                  </div>
+
+                  <div>
+                      <p>4.	Are there any standard symbols used in rainwater harvesting drawings?</p>
+                      <p><strong>Ans:</strong> Yes, there are specific symbols and common ways of drawing things in <a href="https://www.rainyfilters.com/about-us/blogs/types-of-filter">rainwater harvesting diagrams</a>. These symbols help show things like tanks, filters, and pipes in a clear and understandable way.</p>
+                  </div>
+
+                  <div>
+                      <p>5.	Can rainwater harvesting drawings be modified or updated after implementation?</p>
+                      <p><strong>Ans:</strong> Certainly, you can change or improve rainwater harvesting drawings if you are looking to make any changes or add things to the system over time.</p>
+                  </div>
+
+
+              </div>
+
+
+              
+            </div>
+          </div>
+
+          {/* <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+						Launch demo modal
+					</button> */}
+
+
+          <Footer />
+        </div>
+      </main>
+
+      <footer />
+    </Fragment>
+  );
+}
