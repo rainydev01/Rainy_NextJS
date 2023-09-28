5eeff23d299c4d2ccda81905ae0e82407ec6c983
Prashant <Khanayatprashant@gmail.com>
2023-09-28 23:30:25 +0530

    added two new page

---
+++ pages/roof-top-rain-water-harvesting.js
@@ -0,0 +1,224 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function RoofTopRainWaterHarvesting() {
+  return (
+    <Fragment>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+														  window.dataLayer = window.dataLayer || [];
+  														  function gtag(){dataLayer.push(arguments);}
+  														  gtag('js', new Date());
+															gtag('config', 'UA-168445645-1');
+        `,
+          }}
+        />
+
+        <title> Roof top Rain Water Harvesting (RRWH) | Rainyfilters.com </title>
+        <link rel="canonical" href="https://www.rainyfilters.com/roof-top-rain-water-harvesting" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossOrigin="anonymous"
+        />
+        <meta name="description" content="Rooftop rainwater harvesting (RRWH)  is one of the best ways to the financial savings, environmental responsibility, and address water scarcity."/>
+          <meta name="keywords" content="Roof top Rain Water Harvesting, about Roof top Rain Water Harvesting" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Open+Sans&family=Raleway&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: function googleTranslateElementInit() {
+              /* eslint-disable no-new */
+              new window.google.translate.TranslateElement(
+                {
+                  pageLanguage: "pt",
+                },
+                "google_translate_element"
+              );
+            },
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '587685655184685');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
+/>`,
+          }}
+        />
+      </Head>
+      <main>
+        <div className="App">
+          <Header />
+
+          <div className="container-fluid banner enternal">
+            <div className="hero-image enthroimg">
+              {/*<img src="../static/images/rain-water-harvesting-drawing/banner.png" />*/}
+              
+            </div>
+          </div>
+
+          
+            <div className="container">
+                <div className="groundwater_bg team blue_underline">
+                        <p className="team-heading media-hdng blue_underline" style={{ textAlign: "center", font: "" }} >
+                        {/* STORIES , INSIGHTS , and ANNOUNCEMENTS FROM RAINY */}
+                        </p>
+                </div>
+
+                <div className="blog-content">
+                    <div className="blog-info">
+                      <h1>Rooftop rainwater harvesting </h1>
+                      <p>
+                      Rooftop rainwater harvesting is the practice of collecting rainwater and using the water for various other purposes. This involves the collection of the rainwater that falls on the roof area of the buildings, which is then diverted to a rainwater filter through a downspout pipe. The filtered water is then stored in a sump or other water storage tank. It is also possible to recharge groundwater using this method. 71 % of Earth is covered in water and only a small amount of it is fresh water, unfortunately most of the fresh water is not easy to obtain. This leads to a scarcity of fresh water. By conserving rainwater, we can reduce our water bills, which is economically beneficial. RWH also helps us overcome climate change by reducing our carbon and water footprints. By collecting the rainwater during the wet seasons, we can utilize the collected rainwater during the drought periods.
+                      </p>
+                      <p>Rooftop rainwater harvesting systems are a sustainable solution to address problems like water scarcity. RWH helps us reduce dependence on secondary water resources like municipal water supplies, borewells, and ponds and also helps us overcome drought situations.</p>
+                      <div className="blogImg hero-image enthroimg">
+                            <img src="../../../static/images/roof-top-rain-water-harvesting-inner1.png"></img>
+                      </div>
+                    </div>
+
+                    <div className="blog-info">              
+                        <h2>Rainwater Harvesting </h2>
+                        <p> Rainwater harvesting is the practice of collecting rainwater instead of letting it to runoff. Which includes collection, storage and use of the rainwater that falls on the roof, surfaces or catchment areas. This system helps us to our dependency on external water sources, helps us to reduce additional water costs and also helps us to overcome water scarcity.
+                        </p>                        
+                    </div>
+
+                    <div className="blog-info">              
+                        <h2>What is Rooftop Rainwater Harvesting? </h2>
+                        <p> Rooftop rainwater harvesting is a method of collecting and storing the rainwater that falls on the roof of your house or buildings .
+                        </p>
+                        <div className="blogImg hero-image enthroimg">
+                            <img src="../../../static/images/roof-top-rain-water-harvesting-inner2.png"></img>
+                        </div>
+                        <p>The key components of a Rooftop Rainwater Harvesting system include:</p>
+                        <ul style={{ listStyleType: "number"}}>
+                            <li>
+                                <p><span style={{ textDecoration: "underline", fontWeight: "600"}}>Catchment:</span> The area from which rainwater is collected and channelled to storage. The catchment area is one of the key components of Rooftop rainwater harvesting systems because we can determine the amount of water that can be collected and stored. <br/> The efficiency of the catchment area depends upon various factors such as type of the surface, size its capacity to capture rainwater.</p>
+                            </li>
+                            <li>
+                                <p><span style={{ textDecoration: "underline", fontWeight: "600"}}>Gutters and Downspouts:</span> Gutters are the channels that are installed along the edges and made of metal, plastic, or other durable materials. They are installed just below the edge of the roof and are slightly inclined to help the water flow toward the downspouts. <br/> Downspouts are also known as downpipes, these are vertical pipes necessary part of the drainage systems. These are attached to the gutters, and their main objective is to direct the rainwater.</p>
+                            </li>
+                            <li>
+                                <p><span style={{ textDecoration: "underline", fontWeight: "600"}}>First-Flush Diverter:</span> A first flush diverter is a crucial component of rainwater harvesting systems that are used to divert and discard the initial runoff from rainfall in order to improve the quality of rainwater collected. This first flow often carries impurities from the catchment surface, which is usually a rooftop, including dust, debris, and pollutants, which may affect the quality of the rainwater collected. In order to collect cleaner rainwater thereafter, the diverter works by capturing the initial flow and directing it away from the storage tank. The diverter closes once the initial flush of water has been redirected, guaranteeing that the rainwater collected is of higher quality for usage like drinking, irrigation, or non-potable applications.</p>
+                            </li>
+                            <li>
+                                <p><span style={{ textDecoration: "underline", fontWeight: "600"}}>Filters:</span> In order to maintain the quality of rainwater collected, filters are crucial parts of rooftop rainwater harvesting systems. Rainwater may contain impurities like debris, leaves, and dust as it washes over roof catchment area. These contaminants are strategically captured by rainwater filters, such as mesh screens and fine filters, preventing them from entering the storage tank. To maintain the quality of the collected water, we must make sure that we are utilizing the best filters available such as 'Rainy Filters'.</p>
+                            </li>
+                            <li>
+                                <p><span style={{ textDecoration: "underline", fontWeight: "600"}}>Storage tanks:</span> Storage tanks are an essential component of a rooftop rainwater harvesting system since they are used to collect and store the harvested rainwater. Depending upon the various requirements and space constraints, storage tanks are available in a wide variety of sizes, designs and qualities. These storage tanks are typically underground; therefore, it is important to ensure that they are of the correct size, especially during the dry seasons when water supply becomes extremely scarce</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="blog-info">
+                        <h2>Benefits of Rooftop Rainwater Harvesting</h2>
+                        
+                        <ul style={{ listStyleType: "disc"}}>
+                            <li>
+                                <p>We can save a significant amount of water by using rooftop rainwater harvesting. On average, we can save up to 154,308 litres of water from a 1200-square-foot rooftop area.<br/>
+                                  <span style={{ textDecoration: "underline", fontWeight: "500"}}>Note:</span> Depending on the rooftop area, annual rainfall, and collection efficiency, the quantity of water may vary.
+                                </p>                                
+                            </li>
+
+                            <li>
+                              <p>Rooftop rainwater collection might require an initial investment, but it can result in cost savings by reducing water costs while improving drought resistance.</p>
+                            </li>
+
+                            <li>
+                                <p>Harvesting rainwater from the rooftop can be used for irrigation which is an environmentally friendly choice that is beneficial for the environment and your garden. You can reduce your dependency on expensive municipal water supplies while providing your garden with naturally soft and chemical-free watering by collecting rainwater for watering your plants.</p>                                
+                            </li>
+
+                            <li>
+                                <p>Rainwater harvesting reduces water demand significantly by collecting and reusing rainwater for non-potable purposes, thereby reducing the demand for municipal supplies and infrastructure.</p>
+                            </li>
+
+                            <li>
+                                <p>Rooftop rainwater collection is an essential method for groundwater recharge.</p>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                    <div className="blog-info">
+                        <h3>Conclusion</h3>
+                        <div className="blogImg hero-image enthroimg">
+                            <img src="../../../static/images/roof-top-rain-water-harvesting-inner3.png"></img>
+                        </div>
+
+                        <p>By collecting rainwater instead of letting it runoff, we can preserve enormous amounts of rainwater and we can reduce our dependency on external water resources such as municipal water supplies and also, we can preserve our environment by recharging groundwater.</p>
+
+                        <p>In order to lead a sustainable and environment-friendly water future rooftop rainwater harvesting is one of the effective and sustainable approaches for the people and the communities to gain their control over the water supplies.</p>
+                        <p>To step towards a more sustainable and water-secure future implementing <a href="https://www.rainyfilters.com/roof-top-rain-water-harvesting">rooftop rainwater harvesting</a> is one of the best ways to the financial savings, environmental responsibility and address water scarcity.</p>
+                    </div>                    
+
+                </div>
+
+
+              
+            </div>
+          
+
+          {/* <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+						Launch demo modal
+					</button> */}
+
+
+          <Footer />
+        </div>
+      </main>
+
+      <footer />
+    </Fragment>
+  );
+}
