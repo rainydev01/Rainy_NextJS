7380c36aca8949a7b168b487f051a296b19aa031
Prashant <Khanayatprashant@gmail.com>
2023-11-07 23:15:44 +0530

    code updated

--- components/imageSliderBlogNew.js
+++ components/imageSliderBlogNew.js
@@ -33,6 +33,54 @@ const ImageSliderBLogNew = () => {
         	<div style={{ backgroundImage: "url(../static/images/homepage/ddropsbg.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "top left", }} >
 
               <div className="container media-main">
+                  
+                  <div className="col-md-4" style={{ marginTop: "30px" }}>
+              			<div className="sec-brdr">
+                			<div className="mdiaimg">
+                  				<img src="../static/images/blogs/world-of-rainwater-harvesting-inner1.png" style={{ height: "196px", }} />
+                			</div>
+                			<div className="mdiacont">
+                  				<div className="grndtext">
+                    				<h3>
+                      					<a href="/about-us/blogs/world-of-rainwater-harvesting" target="_self" >
+										            Harvesting The skies: Introducing the World of Rainwater Harvesting
+                        				<br />
+                      					</a>
+                    				</h3>
+                    				
+                            <p style={{ textAlign: "left", color: "#4c3f43", fontSize: "13px", fontWeight: "500", letterSpacing: "0.5px", }} >
+									          Rainwater, the solution of life, it is a precious resource that sustains every living thing on our planet. It is the essence of existence, yet it is a finite...
+                      				</p>
+
+                    				<div className="col-md-12 mdiasocial">
+                      					<div className="col-md-6 mediasocial">
+                        					<FacebookShareButton url={ "https://www.rainyfilters.com/about-us/blogs/world-of-rainwater-harvesting" } >
+                          						<i className="fa fa-facebook" aria-hidden="true" />
+                        					</FacebookShareButton>
+
+                        					<TwitterShareButton url={ "https://www.rainyfilters.com/about-us/blogs/world-of-rainwater-harvesting" } >
+                          						<i className="fa fa-twitter" aria-hidden="true" />
+                        					</TwitterShareButton>
+
+                        					<WhatsappShareButton url={ "https://www.rainyfilters.com/about-us/blogs/world-of-rainwater-harvesting" } >
+                          						<i className="fa fa-whatsapp" aria-hidden="true" />
+                        					</WhatsappShareButton>
+                      					</div>
+                      					<div className="col-md-6 mediabtn">
+                        					<button>
+                          						<a href="/about-us/blogs/world-of-rainwater-harvesting" target="_self" >
+                            					Read more
+                          						</a>
+                          						{"  "}
+                          						<i class="fa fa-angle-right" aria-hidden="true" />
+                        					</button>
+                      					</div>
+                    				</div>
+                  				</div>
+                			</div>
+              			</div>
+            		  </div>
+                  
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
               			<div className="sec-brdr">
                 			<div className="mdiaimg">
@@ -127,6 +175,8 @@ const ImageSliderBLogNew = () => {
               			</div>
             		  </div>
 
+              </div>
+              <div className="container media-main" style={{ marginTop: "10vh" }}>
 
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
               			<div className="sec-brdr">
@@ -174,8 +224,7 @@ const ImageSliderBLogNew = () => {
                 			</div>
               			</div>
             		  </div>
-              </div>
-              <div className="container media-main" style={{ marginTop: "10vh" }}>
+              
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
               			<div className="sec-brdr">
                 			<div className="mdiaimg">
@@ -269,8 +318,11 @@ const ImageSliderBLogNew = () => {
                 			</div>
               			</div>
             		  </div>
-            
-            
+              </div>
+          </div>
+
+          <div>
+              <div className="container media-main">
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                       <div className="sec-brdr">
                         <div className="mdiaimg">
@@ -317,11 +369,7 @@ const ImageSliderBLogNew = () => {
                         </div>
                       </div>
                   </div>
-              </div>
-          </div>
-
-          <div>
-              <div className="container media-main">
+            
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                       <div className="sec-brdr">
                         <div className="mdiaimg">
@@ -413,8 +461,12 @@ const ImageSliderBLogNew = () => {
                   				</div>
                 			</div>
               			</div>
-            		  </div>              
-            
+            		  </div>
+
+              </div>
+
+              <div className="container media-main" style={{ marginTop: "10vh" }}>
+
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                       <div className="sec-brdr">
                         <div className="mdiaimg">
@@ -460,10 +512,8 @@ const ImageSliderBLogNew = () => {
                         </div>
                       </div>
                   </div>
-              </div>
-
-          		<div className="container media-main" style={{ marginTop: "10vh" }}>
-            	  <div className="col-md-4" style={{ marginTop: "30px" }}>
+              
+            	    <div className="col-md-4" style={{ marginTop: "30px" }}>
               <div className="sec-brdr">
                 <div className="mdiaimg">
                   <img
@@ -540,9 +590,9 @@ const ImageSliderBLogNew = () => {
                   </div>
                 </div>
               </div>
-            		</div>
+            		  </div>
 
-                <div className="col-md-4" style={{ marginTop: "30px" }}>
+                  <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
                         <img
@@ -620,9 +670,14 @@ const ImageSliderBLogNew = () => {
                         </div>
                       </div>
                     </div>
-                </div>
-            
-                <div className="col-md-4" style={{ marginTop: "30px" }}>
+                  </div>
+              </div>
+
+          </div>
+
+          <div>
+              <div className="container media-main">
+                  <div className="col-md-4" style={{ marginTop: "30px" }}>
               <div className="sec-brdr">
                 <div className="mdiaimg">
                   <img
@@ -702,13 +757,9 @@ const ImageSliderBLogNew = () => {
                   </div>
                 </div>
               </div>
-            		</div>
-              </div>
-          </div>
-            
-          <div>
-              <div className="container media-main">
-                  <div className="col-md-4" style={{ marginTop: "30px" }}>
+            		  </div>
+              
+                    <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
                         <img
@@ -787,9 +838,9 @@ const ImageSliderBLogNew = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                    </div>
           
-                  <div className="col-md-4" style={{ marginTop: "30px" }}>
+                    <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
                         <img
@@ -866,7 +917,10 @@ const ImageSliderBLogNew = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                    </div>
+              </div>
+              
+              <div className="container media-main" style={{ marginTop: "10vh" }}>
             
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                 <div className="sec-brdr">
@@ -948,9 +1002,7 @@ const ImageSliderBLogNew = () => {
                   </div>
                 </div>
                   </div>
-              </div>
               
-              <div className="container media-main" style={{ marginTop: "10vh" }}>
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
@@ -1111,7 +1163,11 @@ const ImageSliderBLogNew = () => {
                       </div>
                     </div>
                   </div>
-              
+              </div>
+          </div>
+          
+          <div>
+              <div className="container media-main" >
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
@@ -1188,11 +1244,7 @@ const ImageSliderBLogNew = () => {
                       </div>
                     </div>
                   </div>
-              </div>
-          </div>
-
-          <div>
-              <div className="container media-main" >
+          
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
@@ -1353,7 +1405,11 @@ const ImageSliderBLogNew = () => {
                       </div>
                     </div>
                   </div>
+
+              </div>
               
+              <div className="container media-main" style={{ marginTop: "10vh" }}>
+
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                       <div className="sec-brdr">
                           <div className="mdiaimg">
@@ -1425,9 +1481,7 @@ const ImageSliderBLogNew = () => {
                           </div>
                       </div>
                   </div>
-              </div>
               
-              <div className="container media-main" style={{ marginTop: "10vh" }}>
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
@@ -1586,7 +1640,12 @@ const ImageSliderBLogNew = () => {
                       </div>
                     </div>
                   </div>
-              
+
+              </div>
+          </div>
+
+          <div>
+              <div className="container media-main" >
                   <div className="col-md-4" style={{ marginTop: "30px" }}>
                     <div className="sec-brdr">
                       <div className="mdiaimg">
@@ -1667,9 +1726,10 @@ const ImageSliderBLogNew = () => {
                       </div>
                     </div>
                   </div>
-                
+
               </div>
           </div>
+              
       </Carousel>
       ;
     </Fragment>
---
+++ pages/about-us/blogs/world-of-rainwater-harvesting.js
@@ -0,0 +1,229 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+import React from "react";
+
+export default function World_of_Rainwater_Harvesting_Introduction()
+{
+
+    return(
+    
+    <Fragment>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168445645-1" ></script>
+  
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                                                            window.dataLayer = window.dataLayer || [];
+                                                              function gtag(){dataLayer.push(arguments);}
+                                                              gtag('js', new Date());
+                                                              gtag('config', 'UA-168445645-1');
+          `,
+            }}
+          />          
+          <script
+            dangerouslySetInnerHTML={{
+              __html: function googleTranslateElementInit() {
+                /* eslint-disable no-new */
+                new window.google.translate.TranslateElement(
+                  {
+                    pageLanguage: "pt",
+                  },
+                  "google_translate_element"
+                );
+              },
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '587685655184685');
+                    fbq('track', 'PageView');`,
+                                }}
+            />
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `<img height="1" width="1" style="display:none"
+                            src="https://www.facebook.com/tr?id=587685655184685&ev=PageView&noscript=1"
+                            />`,
+                }}
+            />
+
+            <title>Harvesting Hope: The Art of Rainwater Conservation</title>
+	        <meta name="description" content="Rainwater conservation is a crucial practice that helps us make the most of this valuable natural resource while reducing the strain on traditional water sources."/>
+            <meta name="keywords" content="Rain Water Conservation Project , Rain Water Conservation Methods" />
+
+            <link rel="canonical" href="https://www.rainyfilters.com/about-us/blogs/harvesting-hope-the-art-of-rainwater-conservation"/>
+	        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" />
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+        </Head>
+        <main>
+            <div className="App">
+                {/*<h1> Media</h1>*/}
+                <Header />
+                <div className="container-fluid banner enternal">
+                    <div className="hero-image enthroimg">
+                        <img src="../../../static/images/blogs/world-of-rainwater-harvesting-banner.png" style={{}} />
+                        <div className="hero-text inthemedia">
+                            <h1 style={{ textAlign: "center" }}></h1> 
+                        </div>
+                    </div>
+                </div>
+  
+                <div className="container">
+                    <div className="groundwater_bg team blue_underline">
+                        <p className="team-heading media-hdng blue_underline" style={{ textAlign: "center", font: "" }} >
+                        {/* STORIES , INSIGHTS , and ANNOUNCEMENTS FROM RAINY */}
+                        </p>
+                    </div>
+  
+                    <div className="blog-content">
+                        <div className="blog-info">
+                            <h1>Harvesting The skies: Introducing the World of Rainwater Harvesting</h1>
+                            <p>
+                            Rainwater, the solution of life, it is a precious resource that sustains every living thing on our planet. It is the essence of existence, yet it is a finite resource. As there is an increase in the population and urbanization, the demand for the water also increases, while our natural water sources are also facing depletion and pollution. So, what is the solution? Well, there is a hidden solution from the nature-rainwater. And it is a time to access this liquid gold through the practice of rainwater harvesting.
+                            </p>
+                            <p>In this blog, we will take you on a journey to explore the world of rainwater harvesting. We’ll reveal its simplicity, its incredible benefits and how can you join this revolution to become a water-conscious superhero. So, let’s embark on this watery adventure together!</p>
+                            <div className="blogImg hero-image enthroimg">
+                  				<img src="../../../static/images/blogs/world-of-rainwater-harvesting-inner1.png" ></img>
+                            </div>
+                        </div>
+
+                        <div className="blog-info">
+                            <h2>What is Rainwater Harvesting?</h2>
+                            {/*<h3 style={{ "fontSize": "27px", "marginTop": "30px"}}>Water Scarcity: A Global Crisis</h3>*/}
+                            <p>
+                            Imagine; every time when it rains, nature showers the gift, the abundance of pure and freshwater. Rainwater harvesting is the art of collecting, storing and using this rainwater for the various non-potable purposes. It is like having an own personal reservoir of clean water, directly from the sky!
+                            </p>
+                            <p><a href="https://www.rainyfilters.com/about-us/blogs/rain-water-harvesting">Rainwater harvesting</a> can be simple as collecting rain in the buckets from the garden or as complicated as installing a fully-fledged system to supply the water to your home. The beauty of this process lies in its adaptability - it can be customized to fit your needs and resources.
+                            </p>
+                            
+                        </div>
+
+                        <div className="blog-info">
+                            <h2>Why Rainwater Harvesting Matters?</h2>
+                            <p>
+                            The earth is facing water scarcity. Traditional sources like rivers and lakes are decreasing and groundwater reserves are depleting rapidly. Rainwater harvesting steps is a game changer it offers a wide range of benefits.
+                            </p>
+                            <div className="blogImg hero-image enthroimg">
+                  				<img src="../../../static/images/blogs/world-of-rainwater-harvesting-inner2.png" ></img>
+                            </div>
+                            <ul style={{ "listStyleType" : "decimal" }}>
+                                <li>
+                                    <h3 style={{ "fontSize": "20px" }}>Conserving a Precious Resource:</h3>
+                                    <p>Rainwater harvesting helps us to conserve water, reducing the pressure on the traditional water sources. It is like having a savings account for a rainy day!</p>
+                                </li>
+                                <li>
+                                	<h3 style={{ "fontSize": "20px" }}>Reducing Water Bills:</h3>
+                                    <p>With your own rainwater collecting system, you can reduce on your water bills. It is like receiving a discount on one of your monthly expenses.</p>
+                                </li>
+                                <li>
+                                	<h3 style={{ "fontSize": "20px" }}>Environmental Friendliness:</h3>
+                                    <p>Harvesting rainwater reduces stormwater runoff, which often carries the pollutants into our rivers and oceans. It’s a simple initiative but impactful way to be kind to the environment.</p>
+                                </li>
+                                <li>
+                                	<h3 style={{ "fontSize": "20px" }}>Boosting Garden Glory:</h3>
+                                    <p>Rainwater is pure and soft, making it the best choice for watering plants. Your garden will be thankful with flourishing blooms and lush greenery.</p>
+                                </li>
+                                <li>
+                                	<h3 style={{ "fontSize": "20px" }}>Disaster Preparedness:</h3>
+                                    <p>In the drought areas or where water supply is not reliable, having a rainwater harvesting system is like an insurance policy for your water needs.</p>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="blog-info">
+                            {/*<h3 style={{ "fontSize": "27px"}}>Sustainable agriculture</h3>*/}
+                            <h2>How does Rainwater Harvesting Work?</h2>
+                            <p>
+                            Now we came to know about the importance of rainwater harvesting, let’s dive more on how it all works. At its core, rainwater harvesting is all about collecting rainwater and storing it for the various purposes. Here is simplified process of rainwater harvesting,
+                            </p>
+                            <ul style={{ "listStyleType" : "decimal" }}>
+                                <li>
+                                    <p><h4 style={{ "fontSize": "17px" }}>Collection:</h4> Rainwater is collected from the roof’s, pavements or other catchment surfaces. This can be done using the gutters and downspouts, which directs the rainwater into a storage tank.</p>
+                                </li>
+
+                                <li>
+                                    <p><h4 style={{ "fontSize": "17px" }}>Filtration:</h4> Before storing, the collected rainwater may need to be filtered to remove the debris and contaminants. This is usually done through a series of filter or the screens.</p>
+                                </li>
+
+                                <li>
+                                    <p><h4 style={{ "fontSize": "17px" }}>Storage:</h4> The filtered rainwater can be stored in tanks. These can be above ground, like large barrels or underground for more significant storage.</p>
+                                </li>
+
+                                <li>
+                                    <p><h4 style={{ "fontSize": "17px" }}>Distribution: </h4> The stored rainwater can be used for variety of purposes, including landscape irrigation, flushing toilets and even for washing clothes.</p>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="blog-info">
+                            <h2>Types of Rainwater Harvesting Systems</h2>
+                            <p><a href="https://www.rainyfilters.com/">Rainwater harvesting systems</a> come in various shapes and sizes and you can use this harvested rainwater for watering your garden or other outdoor purposes.</p>
+
+                            <ul style={{ "listStyleType" : "decimal" }}>
+                                <li>
+                                    <p><h5 style={{ "fontSize": "15px" }}>Rain Barrels:</h5> Rain barrels are the simplest and most cost-effective way to collect rainwater. They are typically placed under downspouts to catch rainwater running off the roof. You can also use this harvested water for watering your garden or the other outdoor chores.</p>
+                                </li>
+
+                                <li>
+                                    <p><h5 style={{ "fontSize": "15px" }}>Dry Systems:</h5> These systems are similar to rain barrels but use a dry pipe system. They are particularly useful in the areas with less rainfall and work well for small-scale collection.</p>
+                                </li>
+
+                                <li>
+                                    <p><h5 style={{ "fontSize": "15px" }}>Wet systems:</h5> Wet systems involve directing rainwater from the roof into a storage tank which can be located below the ground. These systems are ideal for areas with more rainfall and can provide a subsequent water supply.</p>
+                                </li>
+
+                                <li>
+                                    <p><h5 style={{ "fontSize": "15px" }}>Green roofs:</h5> Green roofs, also known as living roofs, use a layer of vegetation to absorb rainwater and reduces the runoff. They not only help with rainwater harvesting but also provide insulation and support biodiversity.</p>
+                                </li>
+
+                                <li>
+                                    <p><h5 style={{ "fontSize": "15px" }}>Stormwater Management Systems:</h5> In the urban areas, stormwater management are designed to collect and treat runoff from paved surfaces. These systems can integrate rainwater harvesting to help in reducing flooding and pollution.</p>
+                                </li>
+                            </ul>
+
+                            {/*<div className="blogImg hero-image enthroimg">
+                  				<img src="../../../static/images/blogs/harvesting-hope-the-art-of-rainwater-conservation-inner2.png" ></img>
+                            </div>*/}
+                            
+                        </div>
+
+                        <div className="blog-info">
+                            <h2>Final Thoughts:</h2>
+                            <div className="blogImg hero-image enthroimg">
+                  				<img src="../../../static/images/blogs/world-of-rainwater-harvesting-inner3.png" ></img>
+                            </div>
+                            <p>In a world where there is a increase in water scarcity, rainwater harvesting offers a sustainable, environmental friendly and cost-effective solution. It empowers the individuals to take control on their water usage and reduce their carbon footprint. So, why wait? Start rainwater harvesting journey today and be a part of the solution. Embrace the rainwater and let your actions ripple through the community and world, by creating a brighter and water-secure future for all of us.</p>
+                            <p>Remember, every drop counts and together we can make a significant impact. join the <a href="https://www.rainyfilters.com/about-us/our-journey">rainwater harvesting revolution</a> and be a hero for both of your wallet and planet.</p>
+                            
+                        </div>
+
+                    </div>
+                </div>
+                {/* {newsArticle} */}
+                <Footer />
+            </div>
+        </main>
+  
+        <footer />
+    </Fragment>
+
+    );
+}
