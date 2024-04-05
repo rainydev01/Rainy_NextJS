45d84fe1d651868f5789c67ecd2f9f303614c428
Nitin Bhalala <bhalalanitin5291@gmail.com>
2024-04-05 12:58:24 +0530

    issue fixed

--- components/Popup.js
+++ components/Popup.js
@@ -1,4 +1,9 @@
+
 import React, { useState, useEffect } from 'react';
+import { MdClose } from 'react-icons/md';
+import axios from 'axios';
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 const Popup = () => {
     const [showPopup, setShowPopup] = useState(false);
@@ -14,7 +19,7 @@ const Popup = () => {
         if (lastPopupShownDate !== currentDate) {
             setShowPopup(true);
             localStorage.setItem('lastPopupShownDate', currentDate);
-            // localStorage.setItem('lastPopupShownDate', '2024-04-05');
+            // localStorage.setItem('lastPopupShownDate', '2024-04-06');
         }
     }, []);
 
@@ -33,7 +38,7 @@ const Popup = () => {
 
     const popupStyles = {
         backgroundColor: '#fff',
-        borderRadius: '5px',
+        borderRadius: '50px',
         boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
         position: 'relative',
         zIndex: 1001
@@ -46,15 +51,15 @@ const Popup = () => {
         background: 'none',
         cursor: 'pointer',
         fontSize: '30px',
+        padding: '10px',
         display: 'flex',
         justifyContent: 'end'
-
     };
 
-    const contentStyles = {
-        width: '880px',
-        margin: 'auto',
-    };
+    // const contentStyles = {
+    //     width: '950px',
+    //     margin: 'auto',
+    // };
 
     const [formData, setFormData] = useState({
         name: "",
@@ -88,10 +93,13 @@ const Popup = () => {
                     "https://739o33ovd6.execute-api.ap-south-1.amazonaws.com/Production/",
                     emailToSend
                 );
-                console.log(result);
-                setShowSuccess(true);
+                console.log("🚀 ~ onSubmit ~ result:", result)
+                // toast.success("Thanks for writing to us, we will get back to you shortly", {
+                //     position: toast.POSITION.TOP_RIGHT,
+                // });
+                // setShowSuccess(true);
             } catch (err) {
-                console.error(err);
+                console.log("🚀 ~ onSubmit ~ err:", err)
                 setErrStatus(true);
             }
         }
@@ -131,26 +139,25 @@ const Popup = () => {
     return showPopup ? (
         <div style={overlayStyles}>
             <div style={popupStyles}>
-                <div style={contentStyles}>
-
-
+                <div className='contentStyles'>
                     <div style={{ display: 'flex' }}>
                         <div className='image-section'>
-                            <img src="../static/images/contact-form.png" alt="" style={{ height: '100%' }} />
+                            <img src="../static/images/contact-form.png" alt="" className='imagheight' />
                         </div>
                         <div className="form-section">
                             <div className='colseBtn'>
-                                <button style={closeButtonStyles} onClick={closePopup}>
-                                    &times;
-                                </button>
+                                <div style={closeButtonStyles} onClick={closePopup}>
+                                    {/* &times; */}
+                                    <MdClose />
+                                </div>
                             </div>
                             <div className="gtintchrgt">
-                                <p className='form-heading'>
+                                <p className='form-heading my-heading'>
                                     RWH Consultancy
                                 </p>
 
                                 <form onSubmit={onSubmit}>
-                                    <div className="fominpt">
+                                    <div className="fominpt myinput">
                                         <input
                                             value={name}
                                             onChange={onChange}
@@ -160,7 +167,7 @@ const Popup = () => {
                                             required
                                         />
                                     </div>
-                                    <div className="fominpt">
+                                    <div className="fominpt myinput">
                                         <input
                                             value={phone}
                                             onChange={onChange}
@@ -170,7 +177,7 @@ const Popup = () => {
                                             required
                                         />
                                     </div>
-                                    <div className="fominpt">
+                                    <div className="fominpt myinput">
                                         <input
                                             value={email}
                                             onChange={onChange}
@@ -180,7 +187,7 @@ const Popup = () => {
                                             required
                                         />
                                     </div>
-                                    <div className="fominpt frmmsg">
+                                    <div className="fominpt frmmsg myinput1 ">
                                         <input
                                             value={message}
                                             onChange={onChange}
@@ -190,7 +197,7 @@ const Popup = () => {
                                             required
                                         />
                                     </div>
-                                    <div className="fominpt frmbtn">
+                                    <div className="fominpt frmbtn mybutton">
                                         <div style={{ textAlign: 'center' }}>
                                             <input type="submit" name="submit" value="Book a Free Session" />
                                         </div>
