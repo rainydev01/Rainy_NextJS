fadf956e58759224a2681431fe7d1f6d2cc92f84
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-01-15 16:40:26 +0530

    img removal

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ const nextConfig = {
   async redirects() {
     return [
       // Existing Redirections
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'rainyfilters.com',
-          },
-        ],
-        destination: 'https://www.rainyfilters.com/:path*', 
-        permanent: true, // Permanent redirect (301)
-      },
       { source: '/gallery', destination: '/Gallery/gallery', permanent: true },
       { source: '/about-us/blogs/rain-water-', destination: '/about-us/blogs', permanent: true },
       { source: '/contact-us/videoGallery', destination: '/Gallery/videoGallery', permanent: true },
